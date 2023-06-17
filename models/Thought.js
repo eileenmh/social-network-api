73dc1b8911789b81fc1447cc1ea6f456bbc276ba
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-06-17 13:09:36 -0400

    add virtual to Thought model to count reactions

--- models/Thought.js
+++ models/Thought.js
@@ -1,22 +1,33 @@
 const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    maxLength: 128,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 128,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (time) => new Date(time).toLocaleDateString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (time) => new Date(time).toLocaleDateString(),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", thoughtSchema);
