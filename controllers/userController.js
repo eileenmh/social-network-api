0d039c635671d06d598e8a323dda996ecfa42937
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-06-17 14:32:37 -0400

    create addFriend route and populate friends in getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,9 @@ module.exports = {
   // get a single user by its _id and populated thought and friend data
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId });
+      const user = await User.findOne({ _id: req.params.userId }).populate(
+        "friends"
+      );
 
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
@@ -27,15 +29,35 @@ module.exports = {
   },
 
   // create a new user
-  // Example data:
-  // {
-  // "username": "lernantino",
-  // "email": "lernantino@gmail.com"
-  // }
+  /**** Example data:
+      {
+      "username": "lernantino",
+      "email": "lernantino@gmail.com"
+      }
+  ****/
   async createUser(req, res) {
     try {
       const user = await User.create(req.body);
       res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // add friend to a user
+  async addFriend(req, res) {
+    try {
+      const updateUser1 = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $push: { friends: req.params.friendId } },
+        { new: true }
+      );
+      const updateUser2 = await User.findOneAndUpdate(
+        { _id: req.params.friendId },
+        { $push: { friends: req.params.userId } },
+        { new: true }
+      );
+      res.json({ message: "Friend added" });
     } catch (err) {
       res.status(500).json(err);
     }
